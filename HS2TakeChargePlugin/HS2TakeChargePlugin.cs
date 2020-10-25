5c0dbfb266dda976dd492232289c4b40c1953c4c
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2020-10-24 23:17:59 -0600

    Got auto mode working

--- HS2TakeChargePlugin/HS2TakeChargePlugin.cs
+++ HS2TakeChargePlugin/HS2TakeChargePlugin.cs
@@ -14,11 +14,13 @@ using UnityEngine;
 namespace HS2TakeChargePlugin
 {
     [BepInPlugin(GUID,  PluginName, Version)]
+    [BepInProcess("HoneySelect2.exe")]
+    [BepInProcess("HoneySelect2VR.exe")]
     public class HS2TakeChargePlugin : BaseUnityPlugin
     {
         public const string GUID = "orange.spork.hs2takechargeplugin";
         public const string PluginName = "HS2TakeChargePlugin";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         public static ConfigEntry<KeyboardShortcut> TakeChargeKey { get; set; }
         public static ConfigEntry<KeyboardShortcut> AutoKey { get; set; }
@@ -47,7 +49,7 @@ namespace HS2TakeChargePlugin
         {
             if (TakeChargeKey.Value.IsUp())
             {
-                if (HSceneFlagCtrl.Instance.initiative == 0)
+                if (Singleton<HSceneFlagCtrl>.Instance.initiative == 0)
                 {
                     Log.LogMessage("Take Charge: Active");                    
                 }
@@ -55,19 +57,20 @@ namespace HS2TakeChargePlugin
                 {
                     Log.LogMessage("Take Charge: Disabled");
                 }
-                HSceneFlagCtrl.Instance.click = HSceneFlagCtrl.ClickKind.LeaveItToYou;
+                Singleton<HSceneFlagCtrl>.Instance.click = HSceneFlagCtrl.ClickKind.LeaveItToYou;
             }
             if (AutoKey.Value.IsUp())
             {
-                if (HSceneFlagCtrl.Instance.initiative == 0)
+                if (Singleton<HSceneFlagCtrl>.Instance.initiative == 0)
                 {
                     Log.LogMessage("Auto H: Enabled");
+                    Singleton<HSceneFlagCtrl>.Instance.initiative = 2;
                 }
                 else
                 {
                     Log.LogMessage("Auto H: Disabled");
-                }
-                HSceneFlagCtrl.Instance.initiative = 2;
+                    Singleton<HSceneFlagCtrl>.Instance.initiative = 0;
+                }                
             }            
         }
 
@@ -89,11 +92,91 @@ namespace HS2TakeChargePlugin
 
             MethodInfo houshiCumFix = AccessTools.Method(typeof(Houshi), "AutoOLoopProc", new Type[] { typeof(int), typeof(float), typeof(int), typeof(HScene.AnimationListInfo) }, null);
             harmony.Patch(houshiCumFix, new HarmonyMethod(typeof(HS2TakeChargePlugin), "HoushiCumFix"), null, null, null);
+
+            MethodInfo autoControlMethodSelector = AccessTools.Method(typeof(HAutoCtrl), "GetAnimation", new Type[] { typeof(List<HScene.AnimationListInfo>[]), typeof(int), typeof(bool) }, null);
+            //   harmony.Patch(autoControlMethodSelector, new HarmonyMethod(typeof(HS2TakeChargePlugin), "SelectingAnimationLogger"), new HarmonyMethod(typeof(HS2TakeChargePlugin), "SelectedAnimationLogger"), null, null);
+            harmony.Patch(autoControlMethodSelector, /* new HarmonyMethod(typeof(HS2TakeChargePlugin), "SelectingAnimationLogger") */ null, new HarmonyMethod(typeof(HS2TakeChargePlugin), "SelectedAnimationLogger"), null, null);
+        }
+
+        static void SelectingAnimationLogger(List<HScene.AnimationListInfo>[] _listAnim, int _initiative, bool _isFirst)
+        {
+            ManualLogSource log = HS2TakeChargePlugin.Instance.Log;
+
+        //    log.LogInfo(string.Format("Selecting Animation: Init: {0} isFirst: {1} Player Sex: {2} Futa: {3} FC: {4} MC: {5}", _initiative, _isFirst, HSceneManager.Instance.player.sex, HSceneManager.Instance.bFutanari, HSceneManager.Instance.Hscene.GetFemales()?.Length, HSceneManager.Instance.Hscene.GetMales()?.Length));
+            int i = 0, j = 0, a = 0;
+            foreach (List<HScene.AnimationListInfo> animList in _listAnim)
+            {
+                foreach (HScene.AnimationListInfo ali in animList)
+                {
+             //       log.LogInfo(string.Format("({7}-{5},{0}) Considering Id: {0} Pos: {6} Name: {1} Ctrl1: {2} Ctrl2: {3} Init: {4}", ali.id, ali.nameAnimation, ali.ActionCtrl.Item1, ali.ActionCtrl.Item2, ali.nInitiativeFemale, i, j, a));
+                    j++;
+                    a++;
+                }
+                j = 0;
+                i++;                
+            }
+        }
+
+        static void SelectedAnimationLogger(List<HScene.AnimationListInfo>[] _listAnim, int _initiative, bool _isFirst, ref HScene.StartMotion __result)
+        {
+          //  HS2TakeChargePlugin.Instance.Log.LogInfo(string.Format("Selected {0} with {1}", __result.id, __result.mode));
+            if (_initiative == 2)
+            {
+                __result = RandomSelectAnimation(_listAnim, 3, 4, 5, 6, 7, 8);
+            //    HS2TakeChargePlugin.Instance.Log.LogInfo(string.Format("Overrode to {0} with {1}", __result.id, __result.mode));
+            }
+        }
+
+        static HScene.StartMotion RandomSelectAnimation(List<HScene.AnimationListInfo>[] animList, params int[] excludedCategories)
+        {
+            HAutoCtrl.AutoRandom autoRandom = new HAutoCtrl.AutoRandom();
+
+            bool male = Singleton<HSceneManager>.Instance.player.sex == 0;
+            bool futa = Singleton<HSceneManager>.Instance.bFutanari && !male;
+            bool multipleFemales = Singleton<HSceneManager>.Instance.Hscene.GetFemales().Length > 1;
+            bool fem1Present = Singleton<HSceneManager>.Instance.Hscene.GetFemales()[1] != null;
+            bool multipleMales = Singleton<HSceneManager>.Instance.Hscene.GetMales().Length > 1;
+
+            for (int mode = 0; mode < animList.Length; mode++)
+            {
+                for (int pos = 0; pos < animList[mode].Count; pos++)
+                {
+                    if (animList[mode][pos].nPositons.Contains(Singleton<HSceneFlagCtrl>.Instance.nPlace))
+                    {
+                        // Skip positions not available in location
+                        continue;
+                    }
+                    if (mode == 4 && (male || futa))
+                    {
+                        //Skip masturbation if not female
+                        continue;
+                    }
+                    if ( mode == 5 && (male || futa ) && !fem1Present)
+                    {
+                        // Don't peep without a female subject? 
+                        continue;
+                    }
+                    if (excludedCategories.Contains(mode))
+                    {
+                        // weeding these out
+                        continue;
+                    }
+
+                    // Staying with Illusion Random logic for consistency...
+                    HAutoCtrl.AutoRandom.AutoRandomDate autoRandomDate = new HAutoCtrl.AutoRandom.AutoRandomDate();
+                    autoRandomDate.mode = mode;
+                    autoRandomDate.id = animList[mode][pos].id;
+                    autoRandom.Add(autoRandomDate, 10f);
+                }
+            }
+
+            HAutoCtrl.AutoRandom.AutoRandomDate selectedAutoRandom = autoRandom.Random();
+            return new HScene.StartMotion(selectedAutoRandom.mode, selectedAutoRandom.id);
         }
 
         static void HoushiCumFix(Houshi __instance, HSceneSprite ___sprite)
         {
-            if (HSceneFlagCtrl.Instance.initiative == 1 && HSceneFlagCtrl.Instance.feel_m >= 1f)
+            if (Singleton<HSceneFlagCtrl>.Instance.initiative == 1 && Singleton<HSceneFlagCtrl>.Instance.feel_m >= 1f)
             {
 
                 // Roll random in/out
@@ -102,21 +185,21 @@ namespace HS2TakeChargePlugin
                     float finishRoll = UnityEngine.Random.value;
                     if (___sprite.IsFinishVisible(3) && finishRoll <= .33f)
                     {
-                        HSceneFlagCtrl.Instance.click = HSceneFlagCtrl.ClickKind.FinishDrink;
+                        Singleton<HSceneFlagCtrl>.Instance.click = HSceneFlagCtrl.ClickKind.FinishDrink;
                     }
                     else if (___sprite.IsFinishVisible(4) && (finishRoll > .33f && finishRoll <= .66f))
                     {
-                        HSceneFlagCtrl.Instance.click = HSceneFlagCtrl.ClickKind.FinishVomit;
+                        Singleton<HSceneFlagCtrl>.Instance.click = HSceneFlagCtrl.ClickKind.FinishVomit;
                     }
                     else
                     {
-                        HSceneFlagCtrl.Instance.click = HSceneFlagCtrl.ClickKind.FinishOutSide;
+                        Singleton<HSceneFlagCtrl>.Instance.click = HSceneFlagCtrl.ClickKind.FinishOutSide;
                     }
 
                 }
                 else
                 {
-                    HSceneFlagCtrl.Instance.click = HSceneFlagCtrl.ClickKind.FinishOutSide;
+                    Singleton<HSceneFlagCtrl>.Instance.click = HSceneFlagCtrl.ClickKind.FinishOutSide;
                 }
 
             }
@@ -124,7 +207,7 @@ namespace HS2TakeChargePlugin
 
         static void FixWheelValue(bool _start, ref float _wheel)
         {
-            if (HSceneFlagCtrl.Instance.initiative > 0)
+            if (Singleton<HSceneFlagCtrl>.Instance.initiative > 0)
             {
                 _wheel = 0.1f;
             }
@@ -132,7 +215,7 @@ namespace HS2TakeChargePlugin
 
         static void FixWheelValue2(bool _start, ref float wheel)
         {
-            if (HSceneFlagCtrl.Instance.initiative > 0)
+            if (Singleton<HSceneFlagCtrl>.Instance.initiative > 0)
             {
                 wheel = 0.1f;
             }
@@ -169,13 +252,11 @@ namespace HS2TakeChargePlugin
 
             HSceneManager.HResourceTables.HAutoInfo = autoInfo;
             HSceneManager.HResourceTables.HAutoLeaveItToYou = autoLeaveItToYou;
-            HS2TakeChargePlugin.Instance.Log.LogInfo("Setup HResource Tables");
         }
 
         static void SetupHScene(HScene __instance)
         {
             __instance.ctrlAuto.Load(null, 0);
-            HS2TakeChargePlugin.Instance.Log.LogInfo("HScene Load");
         }
     }
 }
